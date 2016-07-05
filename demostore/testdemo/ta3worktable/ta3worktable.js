0125d7e30d1396e0db01ef3f81fc11b106fec0c9
chenyao <chenyao@yinhai.com>
2016-07-05 16:43:48 +0800

    组装改造

--- demostore/testdemo/ta3worktable/ta3worktable.js
+++ demostore/testdemo/ta3worktable/ta3worktable.js
@@ -12,37 +12,75 @@ var Row = [5, 72, 139, 206, 273, 340, 407, 474, 541, 608, 675, 742, 809,
 //装入盒子容器list
 var conlist = [];
 $(function () {
-
-
-       //$("#add_con").
-        $.each($(".con"), function (i, el) {
-            var drag_el = new DragCon(el);
-            conlist.push(drag_el);
-        });
+    //$("#add_con").
+    $.each($(".con"), function (i, el) {
+        var drag_el = new DragCon(el);
+        conlist.push(drag_el);
+    });
     initAllBoxPosition();
-
-
 });
 
-//$("#add_con").click(function(){
-//
-//    initAllBoxPosition();//创建完成初始化位置
-//
-//});
+$("#add_con").bind("click", function () {
+
+    Mask_con.chooseCreate(function () {
+        //如果是用户选择了创建那么运行创建函数
+        creatDragcon("系统","https://www.baidu.com/",5,2,1,1)
+    });
+})
+
+var Mask_con = {
+    panel_name: "",
+    panel_src: "",
+    chooseCreate: function (callback) {
+        this.openMask();
+        //确认创建那么运行回调函数
+        this.ensureCreate(callback);
+        this.cancelCreate();
+    },
+    openMask: function () {
+        document.getElementById("mask_con").style.display = "block";
+    },
+    cancelCreate: function () {
+        $("#close_mask").click(function () {
+            this.panel_name = "";
+            this.panel_src = "";
+            document.getElementById("mask_con").style.display = "none";
+        })
+    },
+    closeMask: function () {
+        var m_name = document.getElementById("m_name").value,
+            m_src = document.getElementById("m_src").value;
+        this.panel_name = m_name;
+        this.panel_src = m_src;
+        document.getElementById("mask_con").style.display = "none";
+
+    },
+    ensureCreate: function (callback) {
+        var that = this;
+        $("#ensure_create").click(function () {
+            if (typeof (callback) == "function") {
+                that.closeMask();
+                callback();
+            }
+        })
+    }
+}
 
 
-//
-//function  creatDragcon(){
-//
-//
-//}
-//
-//
-//
-//
-//function addCon(){
-//
-//}
+function creatDragcon(type, src, sizeX, sizeY, col, row) {
+    var pl = "  <li class='con' data-sizeX='" + sizeX + "' data-sizeY='" + sizeY + "' data-col='" + col + "' data-row='" + row + "'> " +
+        "<div class='drag_bar'> " +
+        "<span class='con_head_type'>" + type + "</span> " +
+        "<span class='con_head_delete'></span> " +
+        "<span class='con_head_refresh'></span> " +
+        "</div> " +
+        "<div class='con_content'> " +
+        "<iframe  frameborder='0' scrolling='yes' src='" + src + "'></iframe> " +
+        "</div> " +
+        "<span class=resize_bar'><span></span></span> " +
+        "</li>";
+       $(".auto_con").append(pl);
+}
 
 
 function DragCon(el) {
@@ -57,35 +95,31 @@ function DragCon(el) {
     this.col = parseInt($(el).attr("data-col"));//所在多少列
     this.row = parseInt($(el).attr("data-row"));//所在多少行
 
-    this.refresh=$(el).find(".con_head_refresh");//刷新
-    this.delete=$(el).find(".con_head_delete");//删除
-    this.con_content=$(el).find("iframe");
-    this.con_src=$(el).find("iframe").attr("src");
+    this.refresh = $(el).find(".con_head_refresh");//刷新
+    this.delete = $(el).find(".con_head_delete");//删除
+    this.con_content = $(el).find("iframe");
+    this.con_src = $(el).find("iframe").attr("src");
     var that = this;//用于在function里面使用更加方便
     //初始记录鼠标的位置也用来记录鼠标上一次的位置
     this.mouse_left = 0;
     this.mouse_top = 0;
     //refresh  click事件
-    this.refresh.click(function(){
+    this.refresh.click(function () {
         that.con_content.attr('src', that.con_src);
     });
     //delete click事件
-    this.delete.click(function(){
+    this.delete.click(function () {
         //去掉当前节点
-
-        var len=conlist.length;
-        for(var i=0;i<len;i++){
-            console.log(conlist[i],that.el[0])
-            if(conlist[i].el==that.el){
-                console.log("fhsad")
-                conlist.splice(i,1);
+        var len = conlist.length;
+        for (var i = 0; i < len; i++) {
+            console.log(conlist[i], that.el[0])
+            if (conlist[i].el == that.el) {
+                conlist.splice(i, 1);
                 that.el.remove();
                 break;
             }
         }
         initAllBoxPosition();
-
-
     });
 
     //给drag_bar bind mousedown事件
@@ -148,7 +182,7 @@ function DragCon(el) {
         this.el.animate({
             left: col_l + "px",
             top: row_t + "px"
-        },"fast");
+        }, "fast");
     }
     this.resize_bar.bind("mousedown", function (e) {
         that.mouse_left = e.clientX, that.mouse_top = e.clientY;
@@ -205,7 +239,7 @@ function DragCon(el) {
     }
 
     //调整盒子的宽度
-    this.adjustBoxSize=function() {
+    this.adjustBoxSize = function () {
         var sizeX_w = sizeX[that.sizeX - 1];
         var sizeY_h = sizeY[that.sizeY - 1];
         that.el.animate({
@@ -220,22 +254,20 @@ function DragCon(el) {
         that.adjustBoxPosition();
 
     }
+
     init();
 }
 
 function adjustAllBoxPosition() {
-    var clist=conlist;
-    for (var i = 0; i < clist.length; i++)
-    {
-        for (var j = i; j < clist.length; j++)
-        {
-            if (clist[i].row > clist[j].row)
-            {
+    var clist = conlist;
+    for (var i = 0; i < clist.length; i++) {
+        for (var j = i; j < clist.length; j++) {
+            if (clist[i].row > clist[j].row) {
                 var temp = clist[i];
                 clist[i] = clist[j];
                 clist[j] = temp;
             }
-            if(clist[i].row == clist[j].row && clist[j].el.attr("lock")=="locked"){
+            if (clist[i].row == clist[j].row && clist[j].el.attr("lock") == "locked") {
                 var temp = clist[i];
                 clist[i] = clist[j];
                 clist[j] = temp;
@@ -245,8 +277,8 @@ function adjustAllBoxPosition() {
 
     var len = clist.length;
     for (var i = 0; i < len; i++) {
-        var armlist=[];
-        for(var j=0;j<i;j++){//找出当前元素的前面的元素
+        var armlist = [];
+        for (var j = 0; j < i; j++) {//找出当前元素的前面的元素
             if ((clist[j].col <= clist[i].col && clist[i].col < clist[j].col + clist[j].sizeX)
                 || (clist[j].col < clist[i].col + clist[i].sizeX && clist[i].col + clist[i].sizeX < clist[j].col + clist[j].sizeX)
                 || (clist[j].col >= clist[i].col && (clist[j].col + clist[j].sizeX <= clist[i].col + clist[i].sizeX))) {
@@ -270,27 +302,27 @@ function adjustAllBoxPosition() {
 
 
         //box移动过程中，拖拽box不参与位置变动
-        if(!clist[i].el.attr("lock")){
+        if (!clist[i].el.attr("lock")) {
             //如果两次变动结果一直，不打断当前位移动作
-            if( clist[i].row != row){
+            if (clist[i].row != row) {
                 clist[i].el.stop();
                 clist[i].row = row;
-                clist[i].el.attr("data-row",row);
+                clist[i].el.attr("data-row", row);
                 clist[i].adjustBoxPosition();
             }
 
-        }else{
+        } else {
             //拖拽box预选位置变动
             clist[i].row = row;
-            clist[i].el.attr("data-row",row);
-            var li = "<li class='placeholder' style='width:"+sizeX[clist[i].sizeX-1]+"px;height:"+sizeY[clist[i].sizeY-1]+"px;left:"+Col[clist[i].col-1]+"px;top:"+Row[clist[i].row-1]+"px'></li>";
+            clist[i].el.attr("data-row", row);
+            var li = "<li class='placeholder' style='width:" + sizeX[clist[i].sizeX - 1] + "px;height:" + sizeY[clist[i].sizeY - 1] + "px;left:" + Col[clist[i].col - 1] + "px;top:" + Row[clist[i].row - 1] + "px'></li>";
             $(".placeholder").remove();
             $(".auto_con").append(li);
         }
         //释放box时，拖拽box也要加入布局中
-        if(clist[i].el.attr("rlock")){
+        if (clist[i].el.attr("rlock")) {
             clist[i].row = row;
-            clist[i].el.attr("data-row",row);
+            clist[i].el.attr("data-row", row);
             clist[i].adjustBoxPosition();
         }
 
@@ -298,14 +330,11 @@ function adjustAllBoxPosition() {
     }
 }
 
-function initAllBoxPosition(){
-    var clist=conlist;
-    for (var i = 0; i < clist.length; i++)
-    {
-        for (var j = i; j < clist.length; j++)
-        {
-            if (clist[i].row > clist[j].row)
-            {
+function initAllBoxPosition() {
+    var clist = conlist;
+    for (var i = 0; i < clist.length; i++) {
+        for (var j = i; j < clist.length; j++) {
+            if (clist[i].row > clist[j].row) {
                 var temp = clist[i];
                 clist[i] = clist[j];
                 clist[j] = temp;
@@ -315,8 +344,8 @@ function initAllBoxPosition(){
 
     var len = clist.length;
     for (var i = 0; i < len; i++) {
-        var armlist=[];
-        for(var j=0;j<i;j++){//找出当前元素的前面的元素
+        var armlist = [];
+        for (var j = 0; j < i; j++) {//找出当前元素的前面的元素
             if ((clist[j].col <= clist[i].col && clist[i].col < clist[j].col + clist[j].sizeX)
                 || (clist[j].col < clist[i].col + clist[i].sizeX && clist[i].col + clist[i].sizeX < clist[j].col + clist[j].sizeX)
                 || (clist[j].col >= clist[i].col && (clist[j].col + clist[j].sizeX <= clist[i].col + clist[i].sizeX))) {
@@ -339,7 +368,7 @@ function initAllBoxPosition(){
         }
 
         clist[i].row = row;
-        clist[i].el.attr("data-row",row);
+        clist[i].el.attr("data-row", row);
         clist[i].adjustBoxPosition();
 
     }
